0b2f8912dea1843bdfc28bab334b95ad4f38c97b
Makai Post <postmakai@gmail.com>
2021-05-13 10:35:33 -0700

    changed time format

--- client/src/components/LogRow.js
+++ client/src/components/LogRow.js
@@ -1,5 +1,12 @@
 import Moment from 'react-moment'
 
+function toTimeFormat(hours, pad) {
+    const h = Math.floor(hours).toString().padStart(pad, ' ')
+    const m = Math.round(hours % 1 * 60).toString().padStart(2, '0')
+    const time = h + ':' + m
+    return time
+}
+
 function LogRow({id, start, end, hours, notes, date, total, span, editLog}) {
     const dateFormat = 'yy-MM-DD[\n]dddd'
     const timeFormat = 'HH:mm'
@@ -11,9 +18,9 @@ function LogRow({id, start, end, hours, notes, date, total, span, editLog}) {
             </td>}
             <td onDoubleClick={editLog}><Moment format={timeFormat} date={start}/></td>
             <td onDoubleClick={editLog}>{end && <Moment format={timeFormat}>{end}</Moment>}</td>
-            <td onDoubleClick={editLog}>{hours?.toFixed(2).padStart(5, ' ') || ''}</td>
+            <td onDoubleClick={editLog}>{toTimeFormat(hours, 2)}</td>
             <td className='log-notes-cell' onDoubleClick={editLog}>{notes}</td>
-            {total && <td rowSpan={span || 1}>{total.toFixed(2).padStart(7, ' ')}</td>}
+            {total && <td rowSpan={span || 1}>{toTimeFormat(total, 4)}</td>}
         </tr>
     )
 }
